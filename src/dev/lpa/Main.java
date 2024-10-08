1ae223cf358279ff8dc1c06475e3c66cd534d0f3
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-08 18:55:25 +0200

    add more locations

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -6,8 +6,14 @@ public class Main {
 
   public static void main(String[] args) {
 
-    AdventureGame game = new AdventureGame();
-    game.play("road");
+    String myLocations = """
+      lake, at the edge of Lake Tim,E:ocean,W:forest,S:well house,N:cave
+      ocean, on Tim's beach before an angry sea,W:lake
+      cave,at the mouth of Tim's bat cave,E:ocean,W:forest,S:lake
+      """;
+
+    AdventureGame game = new AdventureGame(myLocations);
+    game.play("lake");
 
     Scanner scanner = new Scanner(System.in);
 
